64ef80a119f95b02bcc91473a063d75292a1e0e3
Pranjal Joshi <joshi.pranjal5@gmail.com>
2017-08-05 00:45:56 +0530

    Adding core functionallity.

--- meetAppServer.py
+++ meetAppServer.py
@@ -22,14 +22,106 @@ import MySQLdb as mdb
 
 ###### Main server #########
 
-PORT = 9404
+##### STATIC_VARIABLES #####
 
+PORT = 9404
 DEBUG = True
+SHOW_RAW_MSGS = False
 NO_OF_CONNECTIONS = 0
+DB_NAME = "meetapp_server"
+DB_USER = "root"
+DB_PASSWD = "linux"
+
+#### Global variables ####
 
 activeUsers = []
 connectionAlreadyExists = False
 replaceCount = 0
+
+
+### Database connection ###
+try:
+    con = mdb.connect("localhost",DB_USER,DB_PASSWD)
+    db = con.cursor()
+except:
+    sys.exit("Error connecting MySQL database")
+
+
+def initDB():
+    print "Initializing MySQL database."
+    db.execute("show databases")
+    a = db.fetchall()
+    a = str(a)
+    if(a.find(DB_NAME) > 0):
+        print "---> Database found.\n"
+    else:
+        print "---> Database not found. creating...\n"
+        db.execute("create database %s" % DB_NAME)
+        db.execute("use %s" % DB_NAME)
+        db.execute("create table registration_table(_id BIGINT NOT NULL AUTO_INCREMENT, registered_number VARCHAR(20) NOT NULL, PRIMARY KEY(_id))")
+        db.execute("create table store_and_fwd_table(_id BIGINT NOT NULL AUTO_INCREMENT, arrive_timestamp VARCHAR(20), expire_timestamp VARCHAR(20), send_to VARCHAR(15), message VARCHAR(4000), PRIMARY KEY(_id))")
+        con.commit()
+
+def onOpen(msg,socket):
+    if msg['type'] == "onOpen":
+        lenghtOfActiveUsers = len(activeUsers)
+        connectionAlreadyExists = False
+        for i in range(0,lenghtOfActiveUsers):
+            if(activeUsers[i].keys()[0] == msg['from']):
+                tempDict = activeUsers[i]
+                tempDict[msg['from']] = socket
+                activeUsers[i] = tempDict
+                connectionAlreadyExists = True
+                if DEBUG:
+                    print "Replacing exisiting connection : %s" % phoneNumberFormatter(msg['from'])
+                break
+        if(not connectionAlreadyExists):
+            tempDict = {}
+            tempDict[msg['from']] = socket
+            activeUsers.append(tempDict)
+            if DEBUG:
+                print "New connection created : %s" % phoneNumberFormatter(msg['from'])
+
+def onRegisterUserRequest(req):
+    if req['type'] == "register":
+        number = str(req['from'])
+        number = phoneNumberFormatter(number)
+        db.execute("use meetapp_server")
+        db.execute("select _id from registration_table where registered_number=%s" % number)
+        data = db.fetchall()
+        if not data:
+            db.execute("insert into registration_table(registered_number) values ('%s')" % number)
+            con.commit()
+            if DEBUG:
+                print "New user registered: %s" % number
+        else:
+            if DEBUG:
+                print "User already exists: %s" % number
+
+def onContactSyncRequest(req, socket):
+    if req['type'] == "syncRequest":
+        number = str(req['from'])
+        total = int(req['total'])
+        phonebook = req['phonebook']
+        existingCount = 0
+        syncedNumbers = {}
+        db.execute("use meetapp_server")
+        for i in range(1,total):
+            checkThisNumber = str(phonebook[str(i)])
+            checkThisNumber = phoneNumberFormatter(checkThisNumber)
+            db.execute("select registered_number from registration_table where registered_number='%s'" % checkThisNumber)
+            data = db.fetchone()
+            if data == None:
+                pass
+            else:
+                existingCount += 1
+                syncedNumbers.update({str(existingCount):str(data[0])})
+        resp = {'from':'server','existingCount':str(existingCount),'type':'syncResponse','syncedNumbers':syncedNumbers}
+        if DEBUG:
+            print resp
+        socket.write_message(resp)
+
+##### WebSocketHandler class #####
 
 class WSHandler(tornado.websocket.WebSocketHandler):
 
@@ -42,31 +134,17 @@ class WSHandler(tornado.websocket.WebSocketHandler):
 
     def on_message(self,message):
         global activeUsers
-        if DEBUG:
+
+        msg = ast.literal_eval(message)
+
+        onOpen(msg,self)
+        onRegisterUserRequest(msg)
+        onContactSyncRequest(msg,self)
+
+        if SHOW_RAW_MSGS:
             print message
-            msg = ast.literal_eval(message)
-            if msg['type'] == "onOpen":
-                lenghtOfActiveUsers = len(activeUsers)
-                connectionAlreadyExists = False
-                for i in range(0,lenghtOfActiveUsers):
-                    print activeUsers[i].keys()[0]
-                    print msg['from']
-                    if(activeUsers[i].keys()[0] == msg['from']):
-                        tempDict = activeUsers[i]
-                        tempDict[msg['from']] = self
-                        activeUsers[i] = tempDict
-                        print "Replacing exisiting socket connection."
-                        connectionAlreadyExists = True
-                        break
-                if(not connectionAlreadyExists):
-                    tempDict = {}
-                    tempDict[msg['from']] = self
-                    activeUsers.append(tempDict)
-                    print activeUsers
-                    print "New connection added."
 
-
-    def on_error(self,error):
+    def on_error(self, error):
         print "Websocket error: " + str(error)
         self.close()
 
@@ -78,6 +156,32 @@ class WSHandler(tornado.websocket.WebSocketHandler):
             print "No of connections: " + str(NO_OF_CONNECTIONS)
 
 
+#### Helper functions ####
+
+def phoneNumberFormatter(number):
+    number = str(number)
+    number = number.replace(" ","")
+    number = number.replace(")","")
+    number = number.replace("(","")
+    number = number.replace("-","")
+    number = number.replace("_","")
+    return number
+
+def sendPingTo(number):
+    lenghtOfActiveUsers = len(activeUsers)
+    for i in range(0,lenghtOfActiveUsers):
+        if(activeUsers[i].keys()[0] == str(number)):
+            tempDict = activeUsers[i]
+            socket = tempDict[str(number)]
+            pingFrame = {'from':'server','type':'ping'}
+            socket.write_message(json.dumps(pingFrame))
+            if DEBUG:
+                print "Sent ping to: " + str(number)
+
+//# TODO: Implement pong receive
+
+
+#### Main program ####
 app = tornado.web.Application([(r'/',WSHandler),])
 
 if __name__ == "__main__":
@@ -86,6 +190,7 @@ if __name__ == "__main__":
         print "\n\r[ MeetApp ]"
         print "Started at: " + time.strftime("%d/%m/%y  %I:%M:%S %p")
         print "IP Address: " + str(os.popen("hostname -I").read())
+        initDB()
         try:
             http_server = tornado.httpserver.HTTPServer(app)
             http_server.listen(PORT)
